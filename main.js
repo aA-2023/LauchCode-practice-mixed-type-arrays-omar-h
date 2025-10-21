af665c560b58fe7f56be9df539b7475ad045b49b
aA-2023 <ohern933@gmail.com>
2025-10-20 17:54:08 -0700

    comments for clarity added. reminder: comments are good for when you forget tings

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 let profile = [{userName: `Alice`}, 25, true, {location: `New York`}, [`rock climbing`, `cooking`, `coding`]];
 
-console.log(profile[0].userName, profile[4][1]);
+console.log(profile[0].userName, profile[4][1]);    // displays `Alice` value from object, displays second hobby
+                                                    //string element ` cooking` from hobby array
 
-profile[1] = 19;
+profile[1] = 19;                                    // updates / reassigns age to 19
 
-profile[4].push(`reading`);
+profile[4].push(`reading`);                         //pushes `reading` into nested hobbies array
 
 console.log(profile);
 
